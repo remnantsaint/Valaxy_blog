a5f9153d798fa977e8417c93e18d9f2702203052
remnantsaint <remnantsaint@users.noreply.github.com>
2025-05-22 07:00:22 +0000

    deploy: c88f387e425375e8bf0b5b9296543fa4768cd367

---
+++ assets/YunGallery-Df1CesdM.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./gallery-CE5vU-Tq.js";import"./app-C4FjNBi3.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-BGEDq-bd.js";export{o as default};
---
+++ assets/back-DEkwMenD.js
@@ -0,0 +1,1 @@
+import{b as o}from"./app-C4FjNBi3.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
