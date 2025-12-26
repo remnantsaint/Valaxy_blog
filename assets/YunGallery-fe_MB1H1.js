4e25f2c13e251f8f88a6a9e46a0e438722ce4368
remnantsaint <remnantsaint@users.noreply.github.com>
2025-12-26 01:03:05 +0000

    deploy: 7d712e4443bb83d8d54b3fb693dd41ebba9144c9

---
+++ assets/YunGallery-fe_MB1H1.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./gallery-6JFkZ5yF.js";import"./app-CiB7UBw_.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-CE6MNawq.js";export{o as default};
---
+++ assets/back-DUSjv2yQ.js
@@ -0,0 +1,1 @@
+import{b as o}from"./app-CiB7UBw_.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
